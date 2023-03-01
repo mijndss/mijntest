50c68cff46faa6d3cc0d3224be66014fc5e7d405
mijndss <119042445+mijndss@users.noreply.github.com>
2023-03-01 17:26:56 +0100

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -5,3 +5,6 @@ apt-get install openjdk-19-jre-headless -y
 apt-get install groovy -y
 apt-get install chromium-browser -y
 apt-get install chromium-chromedriver -y
+apt-get install git -y
+
+echo '127.0.0.1       doms' >> /etc/hosts
