411642c7dd00c3291531f6a3fd833cb8466cac53
mijndss <119042445+mijndss@users.noreply.github.com>
2023-03-02 09:32:50 +0100

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -18,5 +18,5 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   
- apt-get update
- apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+ apt-get update -y
+ apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
